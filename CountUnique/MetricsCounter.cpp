92c14048754fa4baa38024882fedb1080687864a
Yotam Salmon <yotam.salmon@gmail.com>
2021-05-17 12:19:30 +0300

    Refactor MetricsCounter

--- CountUnique/MetricsCounter.cpp
+++ CountUnique/MetricsCounter.cpp
@@ -4,39 +4,56 @@
 
 size_t MetricsCounter::comparison_count = 0;
 size_t MetricsCounter::placement_count = 0;
+size_t MetricsCounter::fixed_calculation_count = 0;
 
-void MetricsCounter::add_comparison()
+void MetricsCounter::add_comparison() noexcept
 {
 	comparison_count++;
 }
 
-void MetricsCounter::reset_comparisons()
-{
-	comparison_count = 0;
-}
-
-void MetricsCounter::reset()
-{
-	reset_comparisons();
-	reset_placements();
-}
-
-size_t MetricsCounter::get_comparisons_count()
-{
-	return comparison_count;
-}
-
-void MetricsCounter::add_placement()
+void MetricsCounter::add_placement() noexcept
 {
 	placement_count++;
 }
 
-void MetricsCounter::reset_placements()
+void MetricsCounter::add_fixed_calculation() noexcept
+{
+	fixed_calculation_count++;
+}
+
+void MetricsCounter::reset_all() noexcept
+{
+	reset_comparisons();
+	reset_placements();
+	reset_fixed_calculations();
+}
+
+void MetricsCounter::reset_comparisons() noexcept
+{
+	comparison_count = 0;
+}
+
+void MetricsCounter::reset_placements() noexcept
 {
 	placement_count = 0;
 }
 
-size_t MetricsCounter::get_placements_count()
+void MetricsCounter::reset_fixed_calculations() noexcept
+{
+	fixed_calculation_count = 0;
+}
+
+size_t MetricsCounter::get_comparisons_count() noexcept
+{
+	return comparison_count;
+}
+
+size_t MetricsCounter::get_placements_count() noexcept
 {
 	return placement_count;
+}
+
+size_t MetricsCounter::get_fixed_calculation_count() noexcept
+{
+	return fixed_calculation_count;
 }
--- CountUnique/MetricsCounter.h
+++ CountUnique/MetricsCounter.h
@@ -1,18 +1,37 @@
 #pragma once
 
+/// <summary>
+/// MetricsCounter meters performance of code in terms of placements (includes copies), value comparisons and
+/// fixed calculations (such as hash computation)
+/// </summary>
 class MetricsCounter final
 {
 public:
-	static void add_comparison();
-	static void reset_comparisons();
-	static size_t get_comparisons_count();
+	/// <summary>
+	/// The following functions are used to log when a comparison/placement/fixed calculation takes place.
+	/// It should not normally be called from user code, but is used by Metered<T> types.
+	/// </summary>
+	static void add_comparison() noexcept;
+	static void add_placement() noexcept;
+	static void add_fixed_calculation() noexcept;
 
-	static void add_placement();
-	static void reset_placements();
-	static size_t get_placements_count();
+	/// <summary>
+	/// The following functions reset the meters. reset() must be called before starting measuring an algorithm.
+	/// </summary>
+	static void reset_all() noexcept;
+	static void reset_comparisons() noexcept;
+	static void reset_placements() noexcept;
+	static void reset_fixed_calculations() noexcept;
 
-	static void reset();
+	/// <summary>
+	/// The following functions return the current state of the metrics.
+	/// </summary>
+	static size_t get_comparisons_count() noexcept;
+	static size_t get_placements_count() noexcept;
+	static size_t get_fixed_calculation_count() noexcept;
+
 private:
 	static size_t comparison_count;
 	static size_t placement_count;
+	static size_t fixed_calculation_count;
 };
