6bb1807f0d59d6d8ceec5302e10faebf61d86f46
Yotam Salmon <yotam.salmon@gmail.com>
2021-05-17 14:03:10 +0300

    Add documentation and explanation on hashtable counter

--- CountUnique/HashtableUniqueCounter.h
+++ CountUnique/HashtableUniqueCounter.h
@@ -5,16 +5,28 @@
 #include "UniqueCounter.h"
 #include "MeteredTypes.h"
 
+/// <summary>
+/// Logging hash is a "proxy" hash that logs hash calculations using MetricsCounter. The hash calculations are counted for
+/// MetricsCounter::get_fixed_calculation_count();
+/// </summary>
+/// <typeparam name="OriginalHash">The original hash type</typeparam>
+/// <typeparam name="T">The original type which the original hash operates on</typeparam>
 template <class OriginalHash, class T>
 struct logging_hash
 	: private OriginalHash
 {
 public:
-	size_t operator()(const T& _Keyval) const {
-		CALCULATION return OriginalHash::operator()(_Keyval);
+	size_t operator()(const T& key) const {
+		CALCULATION return OriginalHash::operator()(key);
 	}
 };
 
+/// <summary>
+/// This algorithm is in some way similar to CountingSortUniqueCounter, but it uses a hashset with chaining. For any element in the array,
+/// we search for it in the hashtable, and if it's not found, we insert it into the table and increment the counter.
+/// 
+/// It has the advantage of not requiring prior knowledge on the range of values present in the array.
+/// </summary>
 template <class T, class Hasher = std::hash<T>>
 class HashtableUniqueCounter final
 	: public UniqueCounter<T>
