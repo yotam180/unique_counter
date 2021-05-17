5c939ddb17c97f3fe3649551fd1f5a907178fb7b
Yotam Salmon <yotam.salmon@gmail.com>
2021-05-17 13:05:26 +0300

    Refactor a bit more range_contains, treat warnings as errors

--- CountUnique/OriginalAlgorithmUniqueCounter.h
+++ CountUnique/OriginalAlgorithmUniqueCounter.h
@@ -8,21 +8,31 @@ class OriginalAlgorithmUniqueCounter final
 {
 public:
 	size_t count_unique(std::vector<T>& array) override;
+
+private:
+	bool range_contains(std::vector<T>::iterator start, std::vector<T>::iterator end, T value);
 };
 
 template<class T>
 inline size_t OriginalAlgorithmUniqueCounter<T>::count_unique(std::vector<T>& array)
 {
+	const auto unique_start = array.begin();
 	auto unique_end = array.begin();
 
-	for (auto it = array.begin(); it != array.end(); ++it)
+	for (auto current = array.begin(); current != array.end(); ++current)
 	{
-		if (std::find(array.begin(), unique_end, *it) == unique_end)
+		if (!range_contains(unique_start, unique_end, *current))
 		{
-			*unique_end = *it;
+			*unique_end = *current;
 			unique_end++;
 		}
 	}
 
-	return std::distance(array.begin(), unique_end);
+	return std::distance(unique_start, unique_end);
+}
+
+template<class T>
+inline bool OriginalAlgorithmUniqueCounter<T>::range_contains(std::vector<T>::iterator start, std::vector<T>::iterator end, T value)
+{
+	return std::find(start, end, value) != end;
 }
