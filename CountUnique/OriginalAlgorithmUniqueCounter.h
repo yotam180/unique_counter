4e7e25e929c2f6690bbf64c033aa6e1512df5981
Yotam Salmon <yotam.salmon@gmail.com>
2021-05-17 13:07:18 +0300

    Add some documentation to original algorithm

--- CountUnique/OriginalAlgorithmUniqueCounter.h
+++ CountUnique/OriginalAlgorithmUniqueCounter.h
@@ -2,6 +2,12 @@
 
 #include "UniqueCounter.h"
 
+/// <summary>
+/// Uses the original algorithm presented in Maman 12 to find the number of unique elements in the array.
+/// Stores a subarray of unique elements in the beginning and maintains it throughout the loop, then returns
+/// its size.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 template <class T>
 class OriginalAlgorithmUniqueCounter final
 	: public UniqueCounter<T>
@@ -10,6 +16,7 @@ public:
 	size_t count_unique(std::vector<T>& array) override;
 
 private:
+	// Helper routine that is used to check if the subarray of unique elements already contains an element
 	bool range_contains(std::vector<T>::iterator start, std::vector<T>::iterator end, T value);
 };
 
